7f8d0b2ea3329b4b566246077d2ca20177b6c993
Scott Pakin <pakin@lanl.gov>
2019-04-05 12:42:07 -0600

    Redact all occurrences of the user name in the environment listing
    Thanks to @elliottslaughter for pointing out that including user names
    is incompatible with a double-blind review process.

--- collect_environment.sh
+++ collect_environment.sh
@@ -1,10 +1,16 @@
-#! /bin/sh -x
+#! /bin/sh
+
 # Linux data-gathering commands; adjust as necessary for your platform.
+#
+# Be sure to remove any information from the output that would violate
+# SC's double-blind review policies.
+
+env | sed "s/$USER/USER/g"
+set -x
 lsb_release -a
 uname -a
 lscpu || cat /proc/cpuinfo
 cat /proc/meminfo
-env
 inxi -F -c0
 lsblk -a
 lsscsi -s
